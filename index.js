1fedc9d578ac45eaa27eb98f945ce551791517da
css95 <steilner.carolin@gmail.com>
2026-02-11 16:37:17 +0100

    Add div to html, add class to js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Javascript code
+
+class Pet {
+    constructor(animalType, energy, fullness, happiness) {
+        this.animalType = animalType;
+        this.energy = energy;
+        this.fullness = fullness;
+        this.happiness = happiness;
+    }
+}
+
