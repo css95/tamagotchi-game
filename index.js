51cac6b2202c2756cee61608a931e46de1bfa413
css95 <steilner.carolin@gmail.com>
2026-02-15 15:54:40 +0100

    Update Pet class

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-//Javascript code
-
 class Pet {
-    constructor(animalType, energy, fullness, happiness) {
+    constructor(name, animalType) {
+        this.name = name;
         this.animalType = animalType;
-        this.energy = energy;
-        this.fullness = fullness;
-        this.happiness = happiness;
+        this.energy = 50;
+        this.fullness = 50;
+        this.happiness = 50;
     }
+
+    //Create method for each activity
+    nap() {
+        //Increase energy by 40
+        //Decrease happiness by 10
+        //Decrease fullness by 10
+        //Message
+    }
+
+    play() {
+        //Increase happiness by 30
+        //Decrease fullness by 10
+        //Decrease energy by 10
+        //Message
+    }
+
+    eat() {
+        //Increase fullness by 30
+        //Decrease happiness by 5
+        //Decrease energy by 15
+        //Message
+    }
+
+    //History:
+    //Text box
+
+
 }
 
